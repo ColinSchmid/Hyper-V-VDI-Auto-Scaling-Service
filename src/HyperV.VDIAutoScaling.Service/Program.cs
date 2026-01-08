539189ecb0fefa1074d17a35a962304c394dde0e
Colin Schmid <colin.schmid@bluewin.ch>
2026-01-08 19:05:48 +0100

    Enable Windows Service hosting

---
+++ src/HyperV.VDIAutoScaling.Service/Program.cs
@@ -0,0 +1,8 @@
+using HyperV.VDIAutoScaling.Service;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
